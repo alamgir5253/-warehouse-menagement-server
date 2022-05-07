443c9b339538522c92dd73122530316265b4a856
alomgir hossain <alamgir5253@.com>
2022-05-07 21:16:06 +0600

    create collection to mongodb

--- index.js
+++ index.js
@@ -15,10 +15,18 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.BD_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run(){
   try{
+    client.connect()
+    const CarCollection = client.db("dreamCar").collection("car");
 
+    app.get('/cars', async(req,res) =>{
+      const query ={}
+      const cursor = CarCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
   }
   finally{
-    
+
   }
 
 }
